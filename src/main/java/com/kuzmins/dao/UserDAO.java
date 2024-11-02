ff9b8bfb49e81d1237c5733e6a6ada88c3398885
Olga Kuzmina <o.l.kuzmins@gmail.com>
2024-11-02 23:32:40 +0100

    removed call to ticketDAO from userDAO

--- src/main/java/com/kuzmins/dao/UserDAO.java
+++ src/main/java/com/kuzmins/dao/UserDAO.java
@@ -16,23 +16,23 @@ import java.util.UUID;
 
 @Repository
 public class UserDAO {
-    private final JdbcTemplate jdbcTemplate;
-    private final TicketDAO ticketDAO;
 
     @Value("${switcherForUpdateUserAndCreateTicket}")
     private String switcherForUpdateUserAndCreateTicket;
 
+    private final JdbcTemplate jdbcTemplate;
+
     @Autowired
     public UserDAO(JdbcTemplate jdbcTemplate, TicketDAO ticketDAO) {
         this.jdbcTemplate = jdbcTemplate;
-        this.ticketDAO = ticketDAO;
     }
 
     private final static String INSERT_USER_QUERY = "INSERT INTO user_info (id, name, creation_date, status) VALUES (?, ?, ?, ?::status)";
     private final static String SELECT_USER_BY_ID_QUERY = "SELECT * FROM user_info WHERE id=?";
     private final static String DELETE_USER_BY_ID_QUERY = "DELETE FROM user_info where id=?";
     private final static String UPDATE_USER_STATUS_QUERY = "UPDATE user_info set status=?::status WHERE id=?";
-
+    private static final String SELECT_TICKETS_BY_USER_ID_QUERY = "SELECT * FROM ticket_info WHERE user_id=?";
+    private static final String INSERT_TICKET_WITH_USER_QUERY = "INSERT INTO ticket_info (id, user_id, ticket_type, creation_date) VALUES (?, ?, ?::ticket_type, ?)";
 
     @Transactional
     public void saveUser(User user) {
@@ -44,13 +44,11 @@ public class UserDAO {
                 .stream().findAny().orElse(null);
 
         if (user != null) {
-            List<Ticket> ticketList = ticketDAO.fetchTicketsByUserId(user.getId());
-
-            if (ticketList != null) {
+            List<Ticket> ticketList = jdbcTemplate.query(SELECT_TICKETS_BY_USER_ID_QUERY, new TicketMapper(), new Object[]{user.getId()});
+            if (!ticketList.isEmpty()) {
                 user.setTickets(ticketList);
             }
         }
-
         return user;
     }
 
@@ -65,7 +63,8 @@ public class UserDAO {
             case "ON":
                 Ticket ticket = new Ticket(user.getId(), ticketType);
                 user.getTickets().add(ticket);
-                ticketDAO.saveTicket(ticket, user);
+                jdbcTemplate.update(INSERT_TICKET_WITH_USER_QUERY,
+                                ticket.getId(), user.getId(), ticket.getType().name(), Timestamp.from(ticket.getTicketCreationTime()));
                 jdbcTemplate.update(UPDATE_USER_STATUS_QUERY, Status.ACTIVATED.name(), user.getId());
                 break;
             case "OFF": throw new IllegalArgumentException("The operation is disabled now");
