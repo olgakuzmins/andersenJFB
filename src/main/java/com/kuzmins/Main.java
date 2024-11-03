d70f2483a49d33229b7d8cb3a1f0c287a4a8d60b
Olga Kuzmina <o.l.kuzmins@gmail.com>
2024-11-03 14:10:25 +0100

    added p.7: realisation of reading JSON-file

--- src/main/java/com/kuzmins/Main.java
+++ src/main/java/com/kuzmins/Main.java
@@ -1,16 +1,21 @@
 package com.kuzmins;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kuzmins.config.SpringConfig;
 import com.kuzmins.dao.TicketDAO;
 import com.kuzmins.dao.UserDAO;
+import com.kuzmins.model.BusTicket;
 import com.kuzmins.model.TicketType;
 import com.kuzmins.model.User;
+import com.kuzmins.service.TicketDataReader;
 import com.kuzmins.service.TicketService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
         TicketService ticketService = applicationContext.getBean(TicketService.class);
 
@@ -21,5 +26,8 @@ public class Main {
         userDAO.saveUser(matt);
 
         ticketService.enableUpdateUserAndCreateTicket(matt, TicketType.WEEK);
+
+        TicketDataReader ticketDataReader = applicationContext.getBean(TicketDataReader.class);
+        List<BusTicket> busTicketList = ticketDataReader.ticketsFromJSONDataFile();
     }
 }
